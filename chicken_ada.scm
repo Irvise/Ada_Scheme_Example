3d1722cbf91e36e98acd59b54319e9843a2dc23e
Fernando Oleo Blanco <irvise@protonmail.com>
2021-09-26 19:45:19 +0200

    Improve the scheme side with some comments.

--- chicken_ada.scm
+++ chicken_ada.scm
@@ -3,11 +3,21 @@
 ;; Importing from Ada
 (define hello-in-ada
   (foreign-safe-lambda void "hello_ada" int))
+
+;;;;;;;
+;; These functions are only needed if you are doing launching
+;; tasks and they require some special treatmen (ala advance IPC).
+;; The structure of this two functions is as layed below, however,
+;; the name depends on the name of the library. In our case they
+;; are scheme_hello{init,final}
+;;;;;;;
+
 ;; (define libinit
 ;;   (foreign-safe-lambda void "scheme_helloinit"))
 ;; (define libfinal
 ;;   (foreign-safe-lambda void "scheme_hellofinal"))
 
+;; Exporting from Chicken Scheme
 ;; Callable from the outside, returns an int
 (define-external
   (hello_scheme
@@ -16,7 +26,7 @@
 
   (print "\nHello from Scheme! The message was: \n")
   (print message)
-  (print "\nThe message length was:" (string-length message))
+  (print "\nThe message length was: " (string-length message) "\n")
   (string-length message)
 
   )
